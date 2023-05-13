867f85423a671f5adf5094c3f90196ce4b36743b
Nikita Belousov <nikita.belousov@lightspeedhq.com>
2023-05-13 22:39:20 +0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,21 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svelte()],
+  build: {
+    rollupOptions: {
+      input: 'src/index.ts',
+      output: [
+        {
+          dir: 'dist/cjs',
+          format: 'cjs',
+          sourcemap: true
+        },
+        {
+          dir: 'dist/esm',
+          format: 'esm',
+          sourcemap: true
+        }
+      ],
+    }
+  }
 })
